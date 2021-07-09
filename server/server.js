9cc8072378bd3ec91633c5d10a5b27923be6b16f
FraCav99 <MasterCaster36@outlook.it>
2021-07-09 12:09:39 +0200

    Fix CORS issues

--- server/server.js
+++ server/server.js
@@ -7,9 +7,26 @@ const postRoute = require('./routes/postRoute');
 
 require('dotenv').config();
 
+let whitelist = ['http://localhost:3000'];
+let corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/', postRoute);
+
+// Express error handling triggered when
+// make a request from a 'non-whitelist' origin
+app.use(function (err, req, res, next) {
+  res.json({ err: err.message });
+});
 
 mongoose
   .connect(process.env.DB_CONNECTION_URL, {
@@ -22,4 +39,5 @@ mongoose
     console.log(`An error occur when connect to DB: ${err.message}`)
   );
 
-app.listen(process.env.PORT || 3001, () => console.log('Server is running.'));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
