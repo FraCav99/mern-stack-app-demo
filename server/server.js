a940a181be8d7461e1be5d109c9bf4235115732c
FraCav99 <MasterCaster36@outlook.it>
2021-07-09 15:33:45 +0200

    Fix url endpoint from server side

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const postRoute = require('./routes/postRoute');
 
 require('dotenv').config();
 
-let whitelist = ['https://fracav99.github.io/mern-stack-app-demo'];
+let whitelist = ['https://fracav99.github.io'];
 let corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -22,12 +22,6 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use('/', postRoute);
 
-// Express error handling triggered when
-// make a request from a 'non-whitelist' origin
-app.use(function (err, req, res, next) {
-  res.json({ err: err.message });
-});
-
 mongoose
   .connect(process.env.DB_CONNECTION_URL, {
     useNewUrlParser: true,
@@ -38,6 +32,12 @@ mongoose
   .catch(err =>
     console.log(`An error occur when connect to DB: ${err.message}`)
   );
+
+// Express error handling triggered when
+// make a request from a 'non-whitelist' origin
+app.use(function (err, req, res, next) {
+  res.json({ err: err.message });
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
